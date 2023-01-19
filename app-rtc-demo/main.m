f820f396593ff68ca99e21a3932e5d1549ef13e2
gezhaoyou <gezhaoyou@126.com>
2023-01-19 17:04:59 +0800

    init

--- app-rtc-demo/main.m
+++ app-rtc-demo/main.m
@@ -6,13 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import "ARDAppDelegate.h"
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
-        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+        appDelegateClassName = NSStringFromClass([ARDAppDelegate class]);
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
